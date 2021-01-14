9230d15b6a32194e8b313dcfbef1b3abb8513d35
Andy Dang🤖 <andy@whylabs.ai>
2021-01-13 21:03:01 -0800

    Fixed bug where the whylogs file uploaded is empty
    * Write whylogs profile data to disk
    * Store tags under prefix (whylogs.segment.[tagKey])
    * Store datasetId and orgId in whylogs profile

--- src/main/kotlin/ai/whylabs/services/whylogs/core/Writer.kt
+++ src/main/kotlin/ai/whylabs/services/whylogs/core/Writer.kt
@@ -1,10 +1,11 @@
 package ai.whylabs.services.whylogs.core
 
+import java.nio.channels.FileChannel
+import java.nio.file.Files
 import ai.whylabs.songbird.model.SegmentTag
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.whylogs.core.DatasetProfile
 import org.slf4j.LoggerFactory
-import java.nio.file.Files
 
 interface Writer {
     fun write(profile: DatasetProfile, outputFileName: String, orgId: String, datasetId: String)
@@ -20,15 +21,23 @@ class SongbirdWriter : Writer {
         val tempFile = Files.createTempFile("whylogs", "profile")
         logger.debug("Write profile to temp path: {}", tempFile.toAbsolutePath())
 
-        val tags = profile.tags.map { tag ->
-            SegmentTag().apply {
-                key = tag.key
-                value = tag.value
+        val tags = profile.tags
+            .filterKeys { it.startsWith(SegmentTagPrefix) }
+            .map { tag ->
+                SegmentTag().apply {
+                    key = tag.key.substring(SegmentTagPrefix.length)
+                    value = tag.value
+                }
             }
-        }
 
         try {
-            logger.info("Sending $outputFileName to whylabs $orgId")
+            Files.newOutputStream(tempFile).use {
+                profile.toProtobuf().build().writeDelimitedTo(it)
+            }
+
+            val size = FileChannel.open(tempFile).use { it.size() }
+            val tagString = tags.joinToString(",") { "[${it.key}=${it.value}]" }
+            logger.info("Pushing ${profile.tags[DatasetIdTag]}/$tagString/${profile.dataTimestamp} to WhyLabs $orgId. Size: $size bytes")
             songbirdClientManager.logApi.log(
                 orgId,
                 datasetId,
@@ -37,6 +46,8 @@ class SongbirdWriter : Writer {
                 if (tags.isEmpty()) null else mapper.writeValueAsString(tags),
                 tempFile.toFile()
             )
+            logger.info("Pushed ${profile.tags[DatasetIdTag]}/${tagString}/${profile.dataTimestamp} data successfully")
+
         } finally {
             logger.debug("Clean up temp file: {}", tempFile)
             Files.deleteIfExists(tempFile)
