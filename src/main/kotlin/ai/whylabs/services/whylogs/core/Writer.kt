5f84cec6db9cdc68823dac63eb209005993bfabb
Anthony Naddeo <anthony@whylabs.ai>
2021-10-07 12:00:43 -0700

    Switch to logAsync and remove internal songbird client repo
    We were already using the publicly available songbird client from maven
    so this must have been left over from earlier development.
    
    Now the container will use the logAsync api instead of the log one.

--- src/main/kotlin/ai/whylabs/services/whylogs/core/Writer.kt
+++ src/main/kotlin/ai/whylabs/services/whylogs/core/Writer.kt
@@ -1,6 +1,7 @@
 package ai.whylabs.services.whylogs.core
 
 import ai.whylabs.service.invoker.ApiException
+import ai.whylabs.service.model.LogAsyncRequest
 import ai.whylabs.service.model.SegmentTag
 import com.github.michaelbull.retry.policy.RetryPolicy
 import com.github.michaelbull.retry.policy.fullJitterBackoff
@@ -11,8 +12,6 @@ import com.whylogs.core.DatasetProfile
 import org.slf4j.LoggerFactory
 import java.net.HttpURLConnection
 import java.net.URL
-import java.nio.channels.FileChannel
-import java.nio.file.Files
 
 interface Writer {
     suspend fun write(profile: DatasetProfile, orgId: String, datasetId: String)
@@ -35,40 +34,20 @@ class SongbirdWriter : Writer {
             }
 
         try {
-            // TODO Delete this block once we can use our newer api.
-            "Use the old API".let {
-                val tempFile = Files.createTempFile("whylogs", "profile")
-                Files.newOutputStream(tempFile).use {
-                    profile.toProtobuf().build().writeDelimitedTo(it)
-                }
-
-                retry(retryPolicy) {
-                    songbirdClientManager.logApi.log(
-                        orgId,
-                        datasetId,
-                        profile.dataTimestamp.toEpochMilli(),
-                        tags,
-                        null,
-                        tempFile.toFile()
-                    )
-                }
+            val uploadResponse = retry(retryPolicy) {
+                songbirdClientManager.logApi.logAsync(
+                    orgId,
+                    datasetId,
+                    LogAsyncRequest()
+                        .datasetTimestamp(profile.dataTimestamp.toEpochMilli())
+                        .segmentTags(tags)
+                )
             }
 
-            // TODO uncomment to switch over to the newer api once we finish it.
-//            val uploadResponse = retry(retryPolicy) {
-//                songbirdClientManager.logApi.logAsync(
-//                    orgId,
-//                    datasetId,
-//                    profile.dataTimestamp.toEpochMilli(),
-//                    tags,
-//                    null
-//                )
-//            }
-//
-//            retry(retryPolicy) {
-//                uploadToUrl(uploadResponse.uploadUrl!!, profile)
-//            }
-//
+            retry(retryPolicy) {
+                uploadToUrl(uploadResponse.uploadUrl!!, profile)
+            }
+
             val tagString = if (tags.isEmpty()) "NO_TAGS" else tags.joinToString(",") { "[${it.key}=${it.value}]" }
             logger.info("Pushed ${profile.tags[DatasetIdTag]}/${tagString}/${profile.dataTimestamp} data successfully")
         } catch (e: ApiException) {
