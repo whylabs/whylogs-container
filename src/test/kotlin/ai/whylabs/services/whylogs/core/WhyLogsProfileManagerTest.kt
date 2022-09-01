525e627afc1956bfe1b4c6e2cd385013216f9b80
Anthony Naddeo <anthony@whylabs.ai>
2022-09-01 11:43:53 -0700

    Add errors to the debug info state
    Just another piece of useful info  for remote debugging. The errors are
    already in the logs somewhere else but this will streamline the process
    of debugging and remove some burden from users.

--- src/test/kotlin/ai/whylabs/services/whylogs/core/WhyLogsProfileManagerTest.kt
+++ src/test/kotlin/ai/whylabs/services/whylogs/core/WhyLogsProfileManagerTest.kt
@@ -1,10 +1,6 @@
 package ai.whylabs.services.whylogs.core
 
 import ai.whylabs.services.whylogs.core.config.EnvVars
-import ai.whylabs.services.whylogs.core.config.IEnvVars
-import ai.whylabs.services.whylogs.core.config.ProfileWritePeriod
-import ai.whylabs.services.whylogs.core.config.WriteLayer
-import ai.whylabs.services.whylogs.core.config.WriterTypes
 import ai.whylabs.services.whylogs.core.writer.WriteResult
 import ai.whylabs.services.whylogs.core.writer.Writer
 import ai.whylabs.services.whylogs.persistent.queue.PopSize
@@ -18,32 +14,9 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.Instant
-import java.time.temporal.ChronoUnit
 
 private const val sessionId = "123"
 private const val orgId = "org-1"
-
-private class WhyLabsEnvVars : IEnvVars {
-    override val writer = WriterTypes.WHYLABS
-    override val whylabsApiEndpoint = "none"
-    override val orgId = "org-1"
-    override val ignoredKeys: Set<String> = setOf()
-    override val emptyProfilesDatasetIds = emptyList<String>()
-    override val requestQueueingMode = WriteLayer.SQLITE
-    override val requestQueueingEnabled = true
-    override val profileStorageMode = WriteLayer.SQLITE
-    override val requestQueueProcessingIncrement = PopSize.All
-    override val whylabsApiKey = "key"
-    override val whylogsPeriod = ChronoUnit.HOURS
-    override val profileWritePeriod = ProfileWritePeriod.HOURS
-    override val expectedApiKey = "password"
-    override val s3Prefix = ""
-    override val s3Bucket = "test-bucket"
-    override val port = 8080
-    override val debug = false
-    override val kafkaConfig = null
-    override val fileSystemWriterRoot = "whylogs-profiles"
-}
 
 class WhyLogsProfileManagerTest {
 
@@ -53,7 +26,7 @@ class WhyLogsProfileManagerTest {
     @BeforeEach
     fun init() = runBlocking {
         mockkObject(EnvVars)
-        every { EnvVars.instance } returns WhyLabsEnvVars()
+        every { EnvVars.instance } returns TestEnvVars()
         profileStore = ProfileStore(
             sessionId = sessionId,
         )
