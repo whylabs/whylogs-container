4b3b5a34ec1f6a0f81ff7aacec8c89a4b67688ac
Anthony Naddeo <anthony@whylabs.ai>
2022-08-31 17:03:07 -0700

    Add script to generate swagger docs

---
+++ scripts/get-swagger-def.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+trap 'kill $(jobs -p)' EXIT
+
+DOC_DIR=build/dokka/http
+
+mkdir -p $DOC_DIR
+
+WHYLOGS_PERIOD=HOURS ORG_ID=0 CONTAINER_API_KEY=0 UPLOAD_DESTINATION=DEBUG_FILE_SYSTEM ./gradlew run &
+
+while ! curl -s http://localhost:8080/swagger-docs >/dev/null ; do sleep 1; done ; echo "Server up"
+
+echo "Getting swagger definition from local server."
+DEF=$(curl http://localhost:8080/swagger-docs)
+echo $DEF | tee $DOC_DIR/swagger.json
+
+echo "$(jobs -p)"
